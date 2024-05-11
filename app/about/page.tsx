096a0455d9a6d6e87ecf975925e69831518f098e
SaiKrishnatej <SaiKrishnatej@outlook.in>
2024-05-10 22:23:53 -0700

    lint issues fixed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -42,14 +42,14 @@ const About = () => {
             <p className='mt-4'>
               I have a passion for creating beautiful, functional,
               and user-centered digital experiences. With 7 years of experience in React & Angular. I am always looking for
-              new and innovative ways to bring my clients' visions to life.
+              new and innovative ways to bring my clients visions to life.
             </p>
             <p className='mt-4'>
-              I believe that design is about more than just making things look pretty – it's about solving problems and
+              I believe that design is about more than just making things look pretty its about solving problems and
               creating intuitive, enjoyable experiences for users.
             </p>
             <p className='mt-4'>
-              Whether I'm working on a website, mobile app, or
+              Whether Im working on a website, mobile app, or
               other digital product, I bring my commitment to design excellence and user-centered thinking to
               every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
             </p>
